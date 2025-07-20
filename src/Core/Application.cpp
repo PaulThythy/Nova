583f5dfd386c39eff10e6d2510161d1f4b9ff91f
PaulThythy <paul.thi@hotmail.fr>
2025-07-20 21:07:29 +0200

    modified application.cpp

--- src/Core/Application.cpp
+++ src/Core/Application.cpp
@@ -22,41 +22,6 @@ namespace Nova {
                 std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
                 return;
             }
-
-            // Decide GL+GLSL versions
-            #if defined(IMGUI_IMPL_OPENGL_ES2)
-                // GL ES 2.0 + GLSL 100 (WebGL 1.0)
-                const char* glsl_version = "#version 100";
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-            #elif defined(IMGUI_IMPL_OPENGL_ES3)
-                // GL ES 3.0 + GLSL 300 es (WebGL 2.0)
-                const char* glsl_version = "#version 300 es";
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-            #elif defined(__APPLE__)
-                // GL 3.2 Core + GLSL 150
-                const char* glsl_version = "#version 150";
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-            #else
-                // GL 3.0 + GLSL 130
-                const char* glsl_version = "#version 130";
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-            #endif
-                // other flags
-                SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-                SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-                SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
         }
 
         void Application::initSDL() {
@@ -82,16 +47,41 @@ namespace Nova {
         }
 
         void Application::initImGui() {
-            // GLSL version
+            
+            // Decide GL+GLSL versions
             #if defined(IMGUI_IMPL_OPENGL_ES2)
+                // GL ES 2.0 + GLSL 100 (WebGL 1.0)
                 const char* glslVersion = "#version 100";
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
             #elif defined(IMGUI_IMPL_OPENGL_ES3)
+                // GL ES 3.0 + GLSL 300 es (WebGL 2.0)
                 const char* glslVersion = "#version 300 es";
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
             #elif defined(__APPLE__)
+                // GL 3.2 Core + GLSL 150
                 const char* glslVersion = "#version 150";
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
             #else
+                // GL 3.0 + GLSL 130
                 const char* glslVersion = "#version 130";
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+                SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
             #endif
+                // other flags
+                SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+                SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+                SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
             IMGUI_CHECKVERSION();
             ImGui::CreateContext();
