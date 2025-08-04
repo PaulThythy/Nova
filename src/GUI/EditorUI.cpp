0965d8db4132ed01ad30cb83b312afc903f28de6
PaulThythy <paul.thi@hotmail.fr>
2025-08-04 19:07:05 +0200

    main menu bar with labels (not working yet)

--- src/GUI/EditorUI.cpp
+++ src/GUI/EditorUI.cpp
@@ -42,7 +42,7 @@ namespace Nova::GUI {
         ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
         ImGuiWindowFlags host_window_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
-            ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_UnsavedDocument
+            ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_UnsavedDocument
             | ImGuiWindowFlags_NoResize;
 
         ImGui::Begin("Nova Editor", nullptr, host_window_flags);
@@ -60,6 +60,109 @@ namespace Nova::GUI {
         renderViewportPanel(renderer);
         renderInspectorPanel();
         renderAssetBrowserPanel();
+
+        // Remove border color on main menu bar
+        ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0,0,0,0));
+        if (ImGui::BeginMainMenuBar()) {
+            if (ImGui::BeginMenu("File")) {
+                if (ImGui::MenuItem("New Project...", "Ctrl+Shift+N")) { /* ... */ }
+                if (ImGui::MenuItem("Open Project...", "Ctrl+Shift+O")) { /* ... */ }
+
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("New Scene", "Ctrl+N")) { /* ... */ }
+                if (ImGui::MenuItem("Open Scene...", "Ctrl+O")) { /* ... */ }
+                if (ImGui::MenuItem("Save Scene", "Ctrl+S")) { /* ... */ }
+                if (ImGui::MenuItem("Save Scene As...", "Ctrl+Shift+S")) { /* ... */ }
+
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Import Asset...")) { /* ... */ }
+                if (ImGui::BeginMenu("Export")) {
+                    if (ImGui::MenuItem("Selected as GLTF...")) { /* ... */ }
+                    if (ImGui::MenuItem("Package...")) { /* ... */ }
+                    ImGui::EndMenu();
+                }
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Project Settings...")) { /* ... */ }
+                if (ImGui::MenuItem("Preferences...")) { /* ... */ }
+
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Exit", "Alt+F4")) { /* ... */ }
+
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Edit")) {
+                if (ImGui::MenuItem("Undo", "Ctrl+Z")) { /* ... */ }
+                if (ImGui::MenuItem("Redo", "Ctrl+Y")) { /* ... */ }
+
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Cut", "Ctrl+X")) { /* ... */ }
+                if (ImGui::MenuItem("Copy", "Ctrl+C")) { /* ... */ }
+                if (ImGui::MenuItem("Paste", "Ctrl+V")) { /* ... */ }
+                if (ImGui::MenuItem("Duplicate", "Ctrl+D")) { /* ... */ }
+                if (ImGui::MenuItem("Delete", "Del")) { /* ... */ }
+
+                ImGui::Separator();
+
+                if (ImGui::BeginMenu("Snapping")) {
+                    static bool snapT = true; ImGui::MenuItem("Translate", nullptr, &snapT);
+                    // ...
+                    ImGui::EndMenu();
+                }
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Create")) {
+                if (ImGui::MenuItem("Empty Entity")) { /* ecs create */ }
+                if (ImGui::BeginMenu("3D Object")) {
+                    if (ImGui::MenuItem("Plane")) { /* add MeshComponent.initPlane() */ }
+                    if (ImGui::MenuItem("Sphere")) { /* initSphere(...) */ }
+                    ImGui::EndMenu();
+                }
+                if (ImGui::MenuItem("Camera")) { /* createViewportCamera or regular */ }
+                if (ImGui::BeginMenu("Light")) {
+                    if (ImGui::MenuItem("Directional")) { /* ... */ }
+                    if (ImGui::MenuItem("Point")) { /* ... */ }
+                    if (ImGui::MenuItem("Spot")) { /* ... */ }
+                    ImGui::EndMenu();
+                }
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Window")) {
+                //to configure interface, reset interface, etc.
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Build")) {
+                if (ImGui::MenuItem("Build Project")) { /* ... */ }
+                if (ImGui::MenuItem("Build and Run")) { /* ... */ }
+                if (ImGui::MenuItem("Build Settings")) { /* ... */ }
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Tools")) {
+                if (ImGui::MenuItem("Profiler")) { /* ... */ }
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Help")) {
+                if (ImGui::MenuItem("Documentation")) { /* ... */ }
+                if (ImGui::MenuItem("About Nova")) { /* ... */ }
+                if (ImGui::MenuItem("Check for Updates")) { /* ... */ }
+                if (ImGui::MenuItem("Report Issues")) { /* ... */ }
+                ImGui::EndMenu();
+            }
+
+            // View, Window, Assets, Build, Play, Tools, Help ...
+            ImGui::EndMainMenuBar();
+        }
+        ImGui::PopStyleColor();
     }
 
 } // namespace Nova::GUI
