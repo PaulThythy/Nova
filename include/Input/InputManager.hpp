b6e1692a33837efb76cb8803f025eb51166e9dca
PaulThythy <paul.thi@hotmail.fr>
2025-08-07 22:13:01 +0200

    Adding input manager System

---
+++ include/Input/InputManager.hpp
@@ -0,0 +1,17 @@
+#ifndef INPUT_MANAGER_HPP
+#define INPUT_MANAGER_HPP
+
+namespace Nova::Input {
+
+    class InputManager {
+    public:
+        static InputManager& GetInstance();
+
+        InputManager() = default;
+
+        
+
+    };
+}
+
+#endif//INPUT_MANAGER_HPP
