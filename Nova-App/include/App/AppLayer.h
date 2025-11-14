1c08b2877305409e1e1e7f3ffc183702dd0055f2
PaulThythy <paul.thi@hotmail.fr>
2025-11-14 22:54:26 +0100

    added first opengl rendering in AppLayer

--- Nova-App/include/App/AppLayer.h
+++ Nova-App/include/App/AppLayer.h
@@ -1,16 +1,20 @@
 #ifndef APP_LAYER_H
 #define APP_LAYER_H
 
+#include <glad/gl.h>
+#include <SDL3/SDL.h>
 #include "imgui.h"
 
 #include "Core/Application.h"
 #include "Core/Layer.h"
+#include "Renderer/OpenGL/GL_Shader.h"
 
 namespace Nova::App {
 
     class AppLayer : public Nova::Core::Layer {
     public:
-        AppLayer(Nova::Core::Application& app) : Nova::Core::Layer("AppLayer"), m_App(app) {}
+        explicit AppLayer(Nova::Core::Application& app) : Nova::Core::Layer("AppLayer"), m_App(app) {}
+        ~AppLayer() override;
 
         void OnAttach() override;
         void OnDetach() override;
@@ -22,9 +26,23 @@ namespace Nova::App {
     private:
         Nova::Core::Application& m_App;
 
-        bool m_ShowDemo = true;
-        bool m_ShowAnotherWindow = false;
-        float m_Time = 0.0f;
+        GLuint m_VAO = 0;
+        GLuint m_VBO = 0;
+        GLuint m_SeascapeProgram = 0;
+
+        // uniforms / state
+        float m_Time      = 0.0f;
+        float m_DeltaTime = 0.0f;
+        int   m_Frame     = 0;
+
+        bool  m_MouseDown = false;
+        ImVec2 m_MousePos{0.0f, 0.0f};
+        ImVec2 m_MouseClickPos{0.0f, 0.0f};
+
+        float m_SeaHeight = 0.6f;
+        float m_SeaChoppy = 4.0f;
+        float m_SeaSpeed  = 0.8f;
+        float m_SeaFreq   = 0.16f;
     };
 
 } // namespace Nova::App
--- Nova-App/src/App/AppLayer.cpp
+++ Nova-App/src/App/AppLayer.cpp
@@ -1,35 +1,156 @@
 #include "App/AppLayer.h"
 
+#include <iostream>
+
 namespace Nova::App {
 
-    void AppLayer::OnAttach() {}
-    void AppLayer::OnDetach() {}
-    void AppLayer::OnUpdate(float dt) {}
-    void AppLayer::OnRender() {}
+    AppLayer::~AppLayer() = default;
+
     void AppLayer::OnEvent() {}
 
-    void AppLayer::OnImGuiRender() {
-        if (m_ShowDemo) {
-            ImGui::ShowDemoWindow(&m_ShowDemo);
+    void AppLayer::OnAttach() {
+        // Load shader
+        std::string novaAppRootDir = NOVA_APP_ROOT_DIR;
+        m_SeascapeProgram = Nova::Renderer::OpenGL::LoadRenderShader(
+            novaAppRootDir + "/shaders/OpenGL/seascape/seascape.vert",
+            novaAppRootDir + "/shaders/OpenGL/seascape/seascape.frag"
+        );
+
+        if (!m_SeascapeProgram) {
+            std::cerr << "Failed to load seascape shader program\n";
         }
 
-        {
-            ImGui::Begin("Hello, Nova!");
+        float vertices[] = {
+            // positions   // uv
+            -1.0f, -1.0f,  0.0f, 0.0f,
+             3.0f, -1.0f,  2.0f, 0.0f,
+            -1.0f,  3.0f,  0.0f, 2.0f
+        };
 
-            ImGui::Text("Time: %.3f s", m_Time);
-            ImGui::Checkbox("Show Demo Window", &m_ShowDemo);
-            ImGui::Checkbox("Show Another Window", &m_ShowAnotherWindow);
+        glGenVertexArrays(1, &m_VAO);
+        glGenBuffers(1, &m_VBO);
 
-            ImGui::End();
+        glBindVertexArray(m_VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+    }
+
+    void AppLayer::OnDetach() {
+        if (m_SeascapeProgram) {
+            glDeleteProgram(m_SeascapeProgram);
+            m_SeascapeProgram = 0;
         }
+        if (m_VBO) {
+            glDeleteBuffers(1, &m_VBO);
+            m_VBO = 0;
+        }
+        if (m_VAO) {
+            glDeleteVertexArrays(1, &m_VAO);
+            m_VAO = 0;
+        }
+    }
 
-        if (m_ShowAnotherWindow) {
-            ImGui::Begin("Another Window", &m_ShowAnotherWindow);
-            ImGui::Text("This is another window.");
-            if (ImGui::Button("Close Me")) {
-                m_ShowAnotherWindow = false;
+    void AppLayer::OnUpdate(float dt) {
+        m_Time += dt;
+
+        float mx = 0.0f, my = 0.0f;
+        Uint32 buttons = SDL_GetMouseState(&mx, &my);
+
+        bool leftDown = (buttons & SDL_BUTTON_LMASK) != 0;
+
+        auto& window = m_App.GetWindow();
+        int w, h;
+        window.GetWindowSize(w, h);
+
+        if (leftDown) {
+            // If we just clicked, record the click position
+            if (!m_MouseDown) {
+                m_MouseClickPos = ImVec2((float)mx, (float)my);
             }
-            ImGui::End();
+            m_MouseDown = true;
+            m_MousePos = ImVec2((float)mx, (float)my);
+        } else {
+            m_MouseDown = false;
+            // We keep m_MouseClickPos, but the current position for iMouse.xy will be (0,0)
+            m_MousePos = ImVec2((float)mx, (float)my);
         }
+    }
+
+    void AppLayer::OnRender() {
+        if (!m_SeascapeProgram)
+            return;
+
+        auto& window = m_App.GetWindow();
+        int w, h;
+        window.GetWindowSize(w, h);
+
+        glUseProgram(m_SeascapeProgram);
+
+        // Uniforms Shadertoy
+        GLint locResolution = glGetUniformLocation(m_SeascapeProgram, "iResolution");
+        GLint locTime       = glGetUniformLocation(m_SeascapeProgram, "iTime");
+        GLint locMouse      = glGetUniformLocation(m_SeascapeProgram, "iMouse");
+
+        if (locResolution >= 0)
+            glUniform3f(locResolution, (float)w, (float)h, 1.0f);
+
+        if (locTime >= 0)
+            glUniform1f(locTime, m_Time);
+
+        if (locMouse >= 0) {
+            ImGuiIO& io = ImGui::GetIO();
+
+            float curX = 0.0f;
+            float curY = 0.0f;
+
+            // Si ImGui ne veut PAS la souris et que le bouton gauche est enfoncé,
+            // alors on laisse la caméra du shader utiliser la souris.
+            if (!io.WantCaptureMouse && m_MouseDown) {
+                curX = m_MousePos.x;
+                curY = (float)h - m_MousePos.y; // flip Y pour Shadertoy
+            }
+
+            float clickX = m_MouseClickPos.x;
+            float clickY = (float)h - m_MouseClickPos.y;
+
+            glUniform4f(locMouse, curX, curY, clickX, clickY);
+        }
+
+        // Uniforms de contrôle de la mer
+        GLint locSeaHeight = glGetUniformLocation(m_SeascapeProgram, "uSeaHeight");
+        GLint locSeaChoppy = glGetUniformLocation(m_SeascapeProgram, "uSeaChoppy");
+        GLint locSeaSpeed  = glGetUniformLocation(m_SeascapeProgram, "uSeaSpeed");
+        GLint locSeaFreq   = glGetUniformLocation(m_SeascapeProgram, "uSeaFreq");
+
+        if (locSeaHeight >= 0) glUniform1f(locSeaHeight, m_SeaHeight);
+        if (locSeaChoppy >= 0) glUniform1f(locSeaChoppy, m_SeaChoppy);
+        if (locSeaSpeed  >= 0) glUniform1f(locSeaSpeed,  m_SeaSpeed);
+        if (locSeaFreq   >= 0) glUniform1f(locSeaFreq,   m_SeaFreq);
+
+        glBindVertexArray(m_VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+    }
+
+    void AppLayer::OnImGuiRender() {
+        ImGui::Begin("Seascape Controls");
+
+        ImGui::SliderFloat("Sea Speed",   &m_SeaSpeed,  0.0f, 3.0f);
+        ImGui::SliderFloat("Sea Height",  &m_SeaHeight, 0.0f, 2.0f);
+        ImGui::SliderFloat("Sea Choppy",  &m_SeaChoppy, 0.0f, 8.0f);
+        ImGui::SliderFloat("Sea frequency", &m_SeaFreq, 0.0f, 1.0f);
+
+        ImGui::Text("Time: %.2f", m_Time);
+
+        ImGui::End();
     }
 }
